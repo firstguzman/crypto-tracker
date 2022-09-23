f49030bd48ec5fc611e67ba5b081f8983d086ff9
Cesar Guzman <cesar_guzman2000@hotmail.com>
2022-09-23 10:31:10 -0400

    Fix: error in last commit

--- src/components/favorites/FavoritesScreen.js
+++ src/components/favorites/FavoritesScreen.js
@@ -1,13 +1,21 @@
-import {View} from "react-native"
+import {StyleSheet, View} from "react-native"
 import React from "react"
 import FavoritesEmptyState from "./FavoritesEmptyState"
+import { COLORS } from "../../utils/constants"
 
 const FavoritesScreen = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <FavoritesEmptyState />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.charade
+  }
+})
 
 export default FavoritesScreen
