ba4b181da05818d0ddcfd1ab5a46995ba2530dd6
Cesar Guzman <cesar_guzman2000@hotmail.com>
2022-09-23 10:38:44 -0400

    Ref: create storage library

---
+++ src/libs/storage.js
@@ -0,0 +1,46 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+class Storage {
+  static instance = Storage()
+
+  store = async (key, value) => {
+    try {
+      await AsyncStorage.setItem(key, value)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  get = async key => {
+    try {
+      return await AsyncStorage.getItem(key)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  getAll = async keys => {
+    try {
+      return await AsyncStorage.multiGet(keys)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  getAllKeys = async () => {
+    try {
+      return await AsyncStorage.getAllKeys()
+    } catch (error) {
+      throw error
+    }
+  }
+
+  remove = async key => {
+    try {
+      await AsyncStorage.removeItem(key)
+      return true
+    } catch (error) {
+      return false
+    }
+  }
+}
